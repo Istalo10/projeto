31686c846042c9537205e84d6e7c036832eda0cd
Istalo10 <142236575+Istalo10@users.noreply.github.com>
2023-09-04 09:41:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,5 @@ const overlayImage = document.querySelector('.overlay-image');
 const closeBtn = document.querySelector('.close-btn'); 
 const prevBtn = document.querySelector('.prev-btn'); 
 const nextBtn = document.querySelector('.next-btn'); 
-let currentIndex = 0; images.forEach((image, index) => { image.addEventListener('click', () => { currentIndex = index; showImage(); }); }); 
-closeBtn.addEventListener('click', () => { overlay.style.display = 'none'; }); prevBtn.addEventListener('click', () => { currentIndex = (currentIndex - 1 + images.length) % images.length; showImage(); }); 
-nextBtn.addEventListener('click', () => { currentIndex = (currentIndex + 1) % images.length; showImage(); }); function showImage() 
+let currentIndex = 0; images.forEach((image, index) => { image.addEventListener('click', () => { currentIndex = index; showImage(); }); }); closeBtn.addEventListener('click', () => { overlay.style.display = 'none'; }); prevBtn.addEventListener('click', () => { currentIndex = (currentIndex - 1 + images.length) % images.length; showImage(); }); nextBtn.addEventListener('click', () => { currentIndex = (currentIndex + 1) % images.length; showImage(); }); function showImage() 
 { const imageUrl = images[currentIndex].style.backgroundImage.slice(5, -2); overlayImage.src = imageUrl; overlay.style.display = 'block'; }
